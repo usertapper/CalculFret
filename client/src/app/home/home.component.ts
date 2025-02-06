d2bf7ef141532c1b0dd0f794f1144aee4b62d5d8
Kealohilani <theonathkea@gmail.com>
2025-02-05 17:04:27 -1000

    Mise en forme client

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
